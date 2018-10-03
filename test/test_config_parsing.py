0688254f8caad9c1222adc445f7dbcf79a57b098
emkor93 <emkor93@gmail.com>
2018-10-03 20:05:17 +0200

    added tests and implementation for config parsing

---
+++ test/test_config_parsing.py
@@ -0,0 +1,54 @@
+import json
+import unittest
+
+from todb.config import ToDbConfig
+
+CUSTOM_JSON = """
+{
+  "file": {
+    "encoding": "ascii",
+    "has_header": false,
+    "row_delimiter": ";",
+    "cell_delimiter": "\\t"
+  },
+  "parsing": {
+    "buffer_size_mb": 16,
+    "concurrency": 2
+  },
+  "db": {
+    "type": "pgsql",
+    "host": "pgsql.local",
+    "port": 5432,
+    "user": "root",
+    "password": "secret"
+  }
+}
+"""
+
+
+class ToDbConfigParsingTest(unittest.TestCase):
+    def test_should_return_default_config_on_empty_input(self):
+        default_config = ToDbConfig(conf_dict={})
+        self.assertEqual(default_config.db_host(), ToDbConfig.DEFAULT_DB_HOST)
+        self.assertEqual(default_config.db_port(), ToDbConfig.DEFAULT_DB_PORT)
+        self.assertEqual(default_config.db_user(), ToDbConfig.DEFAULT_DB_USER)
+        self.assertEqual(default_config.db_password(), ToDbConfig.DEFAULT_DB_PASSWORD)
+        self.assertEqual(default_config.file_encoding(), ToDbConfig.DEFAULT_FILE_ENCODING)
+        self.assertEqual(default_config.cell_delimiter(), ToDbConfig.DEFAULT_CELL_DELIMITER)
+        self.assertEqual(default_config.row_delimiter(), ToDbConfig.DEFAULT_ROW_DELIMITER)
+        self.assertEqual(default_config.has_header(), ToDbConfig.DEFAULT_HAS_HEADER)
+        self.assertEqual(default_config.parsing_buffer_size_mb(), ToDbConfig.DEFAULT_PARSING_BUFFER_SIZE_MB)
+        self.assertEqual(default_config.parsing_concurrency(), ToDbConfig.DEFAULT_PARSING_CONCURRENCY)
+
+    def test_should_return_custom_config_on_custom_input(self):
+        custom_config = ToDbConfig(conf_dict=json.loads(CUSTOM_JSON))
+        self.assertEqual(custom_config.db_host(), "pgsql.local")
+        self.assertEqual(custom_config.db_port(), 5432)
+        self.assertEqual(custom_config.db_user(), "root")
+        self.assertEqual(custom_config.db_password(), "secret")
+        self.assertEqual(custom_config.file_encoding(), "ascii")
+        self.assertEqual(custom_config.cell_delimiter(), "\t")
+        self.assertEqual(custom_config.row_delimiter(), ";")
+        self.assertEqual(custom_config.has_header(), False)
+        self.assertEqual(custom_config.parsing_buffer_size_mb(), 16)
+        self.assertEqual(custom_config.parsing_concurrency(), 2)
---
+++ todb/config.py
@@ -0,0 +1,47 @@
+from typing import Any, Text, Dict
+
+
+class ToDbConfig(object):
+    DEFAULT_FILE_ENCODING = "utf-8"
+    DEFAULT_HAS_HEADER = True
+    DEFAULT_ROW_DELIMITER = "\n"
+    DEFAULT_CELL_DELIMITER = ","
+    DEFAULT_PARSING_BUFFER_SIZE_MB = 32
+    DEFAULT_PARSING_CONCURRENCY = 4
+    DEFAULT_DB_HOST = "localhost"
+    DEFAULT_DB_PORT = 3306
+    DEFAULT_DB_USER = "user"
+    DEFAULT_DB_PASSWORD = "password"
+
+    def __init__(self, conf_dict: Dict[Text, Any]) -> None:
+        self.conf_dict = conf_dict
+
+    def file_encoding(self) -> str:
+        return str(self.conf_dict.get("file", {}).get("encoding", self.DEFAULT_FILE_ENCODING))
+
+    def has_header(self) -> bool:
+        return bool(self.conf_dict.get("file", {}).get("has_header", self.DEFAULT_HAS_HEADER))
+
+    def row_delimiter(self) -> str:
+        return str(self.conf_dict.get("file", {}).get("row_delimiter", self.DEFAULT_ROW_DELIMITER))
+
+    def cell_delimiter(self) -> str:
+        return str(self.conf_dict.get("file", {}).get("cell_delimiter", self.DEFAULT_CELL_DELIMITER))
+
+    def parsing_buffer_size_mb(self) -> float:
+        return float(self.conf_dict.get("parsing", {}).get("buffer_size_mb", self.DEFAULT_PARSING_BUFFER_SIZE_MB))
+
+    def parsing_concurrency(self) -> int:
+        return int(self.conf_dict.get("parsing", {}).get("concurrency", self.DEFAULT_PARSING_CONCURRENCY))
+
+    def db_host(self) -> str:
+        return str(self.conf_dict.get("db", {}).get("host", self.DEFAULT_DB_HOST))
+
+    def db_port(self) -> int:
+        return int(self.conf_dict.get("db", {}).get("port", self.DEFAULT_DB_PORT))
+
+    def db_user(self) -> str:
+        return str(self.conf_dict.get("db", {}).get("user", self.DEFAULT_DB_USER))
+
+    def db_password(self) -> str:
+        return str(self.conf_dict.get("db", {}).get("password", self.DEFAULT_DB_PASSWORD))
