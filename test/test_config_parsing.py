f1a4a07cbc6f81f63c2bdf1a4d5a5b74fa1b9a22
emkor93 <emkor93@gmail.com>
2018-10-04 12:03:37 +0200

    fixed imports in tests for todbconfig

--- test/test_config_parsing.py
+++ test/test_config_parsing.py
@@ -1,7 +1,9 @@
 import json
 import unittest
 
-from todb.config import ToDbConfig
+from todb.config import ToDbConfig, DEFAULT_DB_HOST, DEFAULT_DB_PORT, DEFAULT_DB_USER, DEFAULT_DB_PASSWORD, \
+    DEFAULT_DB_DATABASE, DEFAULT_FILE_ENCODING, DEFAULT_CELL_DELIMITER, DEFAULT_ROW_DELIMITER, DEFAULT_HAS_HEADER, \
+    DEFAULT_PARSING_BUFFER_SIZE_kB, DEFAULT_PARSING_CONCURRENCY
 
 CUSTOM_JSON = """
 {
@@ -30,17 +32,17 @@ CUSTOM_JSON = """
 class ToDbConfigParsingTest(unittest.TestCase):
     def test_should_return_default_config_on_empty_input(self):
         default_config = ToDbConfig(conf_dict={})
-        self.assertEqual(default_config.db_host(), ToDbConfig.DEFAULT_DB_HOST)
-        self.assertEqual(default_config.db_port(), ToDbConfig.DEFAULT_DB_PORT)
-        self.assertEqual(default_config.db_user(), ToDbConfig.DEFAULT_DB_USER)
-        self.assertEqual(default_config.db_password(), ToDbConfig.DEFAULT_DB_PASSWORD)
-        self.assertEqual(default_config.db_database(), ToDbConfig.DEFAULT_DB_DATABASE)
-        self.assertEqual(default_config.file_encoding(), ToDbConfig.DEFAULT_FILE_ENCODING)
-        self.assertEqual(default_config.cell_delimiter(), ToDbConfig.DEFAULT_CELL_DELIMITER)
-        self.assertEqual(default_config.row_delimiter(), ToDbConfig.DEFAULT_ROW_DELIMITER)
-        self.assertEqual(default_config.has_header(), ToDbConfig.DEFAULT_HAS_HEADER)
-        self.assertEqual(default_config.parsing_buffer_size_kB(), ToDbConfig.DEFAULT_PARSING_BUFFER_SIZE_kB)
-        self.assertEqual(default_config.parsing_concurrency(), ToDbConfig.DEFAULT_PARSING_CONCURRENCY)
+        self.assertEqual(default_config.db_host(), DEFAULT_DB_HOST)
+        self.assertEqual(default_config.db_port(), DEFAULT_DB_PORT)
+        self.assertEqual(default_config.db_user(), DEFAULT_DB_USER)
+        self.assertEqual(default_config.db_password(), DEFAULT_DB_PASSWORD)
+        self.assertEqual(default_config.db_database(), DEFAULT_DB_DATABASE)
+        self.assertEqual(default_config.file_encoding(), DEFAULT_FILE_ENCODING)
+        self.assertEqual(default_config.cell_delimiter(), DEFAULT_CELL_DELIMITER)
+        self.assertEqual(default_config.row_delimiter(), DEFAULT_ROW_DELIMITER)
+        self.assertEqual(default_config.has_header(), DEFAULT_HAS_HEADER)
+        self.assertEqual(default_config.parsing_buffer_size_kB(), DEFAULT_PARSING_BUFFER_SIZE_kB)
+        self.assertEqual(default_config.parsing_concurrency(), DEFAULT_PARSING_CONCURRENCY)
 
     def test_should_return_custom_config_on_custom_input(self):
         custom_config = ToDbConfig(conf_dict=json.loads(CUSTOM_JSON))
