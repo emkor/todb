25d906f3c21a4ab496ab56cb517b81bfd45615ef
emkor93 <emkor93@gmail.com>
2018-10-04 18:28:35 +0200

    fixed unit test due to interface change on parser, added build badge to readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# todb (work in progress)
+# todb (work in progress) [![Build Status](https://travis-ci.com/emkor/todb.svg?branch=master)](https://travis-ci.com/emkor/todb)
 experimental project: importing csv data into any SQL DB in a smart way
--- test/test_csv_parsing.py
+++ test/test_csv_parsing.py
@@ -8,15 +8,16 @@ from todb.util import proj_path_to_abs
 class CsvParsingTest(unittest.TestCase):
     def test_should_parse_example_csv_file(self):
         abs_csv_path = proj_path_to_abs("resources/example.csv")
-        config = ToDbConfig({"file": {"has_header": True} ,"parsing": {"buffer_size_kB": 1}})
+        config = ToDbConfig({"file": {"has_header": True, }, "parsing": {"buffer_size_kB": 1}})
         parser = CsvParser(config)
 
         all_lines = []
         for lines in parser.read_rows_in_chunks(abs_csv_path):
             all_lines.extend(lines)
-        self.assertIn("Better Person,Zakochany Czlowiek,Zakochany Czlowiek,29 Aug 2018 17:55", all_lines)
-        self.assertIn("Niechęć,[self-titled],Metanol,30 Aug 2018 07:41", all_lines)
-        self.assertIn("Hope Sandoval & The Warm Inventions,Until the Hunter,Into The Trees,30 Aug 2018 12:16",
+        self.assertIn(['Better Person', 'Zakochany Czlowiek', 'Zakochany Czlowiek', '29 Aug 2018 17:55'], all_lines)
+        self.assertIn(['Niechęć', '[self-titled]', 'Rajza', '30 Aug 2018 07:58'], all_lines)
+        self.assertIn(['Hope Sandoval & The Warm Inventions', 'Until the Hunter',
+                       'Into The Trees', '30 Aug 2018 12:16'],
                       all_lines)
-        self.assertNotIn("Artist,Album,Title,Date", all_lines)
+        self.assertNotIn(["Artist", "Album", "Title", "Date"], all_lines)
         self.assertEqual(len(all_lines), 25)
